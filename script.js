44ccdc151f4b2f321dbb536dfb7e714d0e58c52c
rausyanfikrkarmayoga <rosyanxone@student.unmul.ac.id>
2022-05-08 17:31:52 +0800

    Hapus komen js

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const myAlert = document.querySelector('.my-alert');
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-        // Ketika tombol submit diklik
-        // tampilkan tombol loading, hilangkan tombol kirim
     btnLoading.classList.toggle('d-none');
     btnKirim.classList.toggle('d-none');
 
@@ -17,12 +15,9 @@ form.addEventListener('submit', e => {
             body: new FormData(form)
         })
         .then(response => {
-            // Tampilkan tombol kirim, hilangkan tombol loading
             btnLoading.classList.toggle('d-none');
             btnKirim.classList.toggle('d-none');
-            // Tampilkan alert
             myAlert.classList.toggle('d-none');
-            // Reset formnya
             form.reset();
             console.log('Success!', response)
         })
